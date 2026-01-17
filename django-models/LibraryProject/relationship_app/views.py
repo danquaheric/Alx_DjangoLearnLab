6b135f0361758f01387ad2d0ebf4fa8ee8609829
Eric Danquah <danquah@aircellsolutions.com>
2026-01-17 22:45:47 +0000

    Add explicit login import and usage

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.detail import DetailView
 
@@ -21,8 +22,9 @@ def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("login")
+            user = form.save()
+            login(request, user)
+            return redirect("list_books")
     else:
         form = UserCreationForm()
     return render(request, "relationship_app/register.html", {"form": form})
