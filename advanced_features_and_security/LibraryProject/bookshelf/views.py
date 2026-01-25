5eab8f7b09db651957f61153373f65a7fc392e84
Eric Danquah <danquah@aircellsolutions.com>
2026-01-25 17:05:17 +0000

    updated viewa.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,27 +1,21 @@
-# bookshelf/views.py
-
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm, ExampleForm  # ✅ Tracker requires ExampleForm import
+from .forms import BookForm, ExampleForm  
 
-# -----------------------------
-# List / View Books
-# -----------------------------
+
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
-    books = Book.objects.all()  # ORM prevents SQL injection
+    books = Book.objects.all()  
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
-# Optional alias view
+
 @permission_required('bookshelf.can_view', raise_exception=True)
 def view_books(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/view_books.html', {'books': books})
 
-# -----------------------------
-# Create Book (BookForm)
-# -----------------------------
+
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
     form = BookForm(request.POST or None)
@@ -29,9 +23,6 @@ def create_book(request):
         form.save()
     return render(request, 'bookshelf/form_example.html', {'form': form})
 
-# -----------------------------
-# Create Example (ExampleForm)
-# -----------------------------
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_example(request):
     form = ExampleForm(request.POST or None)
@@ -39,9 +30,7 @@ def create_example(request):
         form.save()
     return render(request, 'bookshelf/form_example.html', {'form': form})
 
-# -----------------------------
-# Edit Book
-# -----------------------------
+
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
@@ -50,9 +39,7 @@ def edit_book(request, book_id):
         form.save()
     return render(request, 'bookshelf/edit_book.html', {'form': form, 'book': book})
 
-# -----------------------------
-# Delete Book
-# -----------------------------
+
 @permission_required('bookshelf.can_delete', raise_exception=True)
 def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
