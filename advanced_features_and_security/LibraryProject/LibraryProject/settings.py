1abe71036db39fe579b0ad93b791ac5f263d52b5
Eric Danquah <danquah@aircellsolutions.com>
2026-01-25 15:55:24 +0000

    updated settings.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -53,6 +53,10 @@ SECURE_HSTS_SECONDS = 31536000  # 1 year in seconds
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True  # Apply HSTS to all subdomains
 SECURE_HSTS_PRELOAD = True  # Allows inclusion in browser preload lists
 
+# Trust X-Forwarded-Proto header for HTTPS detection (proxy support)
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
 
 # Application definition
 
