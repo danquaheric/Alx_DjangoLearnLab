3ffea0e79a86bc8d54224634fba67638b5f87737
Eric Danquah <danquah@aircellsolutions.com>
2026-01-25 17:09:50 +0000

    updated viewa.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm, ExampleForm  
+from .forms import BookForm
+from .forms import ExampleForm  
 
 
 @permission_required('bookshelf.can_view', raise_exception=True)
