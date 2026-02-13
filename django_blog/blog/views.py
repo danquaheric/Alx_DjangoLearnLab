701cf4cb7ddd72bb72ed104f3886479ffce37fbd
Eric Danquah <danquah@aircellsolutions.com>
2026-02-13 07:58:59 +0000

    Implementing Advanced Features: Tagging and Search Functionality

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,13 +1,20 @@
+from django.db.models import Q
 from django.views.generic import ListView
 from .models import Post
 
 
-class PostByTagListView(ListView):
+class SearchResultsView(ListView):
     model = Post
-    template_name = 'blog/tag_posts.html'
+    template_name = 'blog/search_results.html'
     context_object_name = 'posts'
 
     def get_queryset(self):
-        return Post.objects.filter(
-            tags__slug=self.kwargs['tag_slug']
-        )
+        query = self.request.GET.get('q')
+        if query:
+            return Post.objects.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(tags__name__icontains=query)
+            ).distinct()
+        return Post.objects.none()
+
