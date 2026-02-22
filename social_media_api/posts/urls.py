326079e424c07e021836812aee0da9a4c1489be0
Eric Danquah <danquah@aircellsolutions.com>
2026-02-22 23:44:36 +0000

    Implementing Notifications and Likes Functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet,  FeedView
+from .views import LikePostView, UnlikePostView
+
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 
 urlpatterns = router.urls + [
+     path('posts/<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
     path('feed/', FeedView.as_view(), name='feed'),
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,8 +1,46 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
+from django.contrib.contenttypes.models import ContentType
+from .models import Post, Like
+from notifications.models import Notification
 
+class LikePostView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, pk):
+        post = Post.objects.get(pk=pk)
+
+        like, created = Like.objects.get_or_create(
+            user=request.user,
+            post=post
+        )
+
+        if created:
+            Notification.objects.create(
+                recipient=post.author,
+                actor=request.user,
+                verb="liked your post",
+                content_type=ContentType.objects.get_for_model(post),
+                object_id=post.id
+            )
+
+        return Response({"message": "Post liked"}, status=status.HTTP_200_OK)
+
+
+class UnlikePostView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, pk):
+        post = Post.objects.get(pk=pk)
+
+        Like.objects.filter(
+            user=request.user,
+            post=post
+        ).delete()
+
+        return Response({"message": "Post unliked"}, status=status.HTTP_200_OK)
 
 class FeedView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
