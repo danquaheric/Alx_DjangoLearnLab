364487062e3d4f05fe7890f681856276b1d6cb2f
Eric Danquah <danquah@aircellsolutions.com>
2026-02-22 23:21:49 +0000

    Implementing User Follows and Feed Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,49 +1,16 @@
-from rest_framework import viewsets, filters
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, permissions
 from rest_framework.response import Response
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
-from .permissions import IsOwnerOrReadOnly
-from rest_framework.pagination import PageNumberPagination
+from .models import Post
+from .serializers import PostSerializer
 
 
-class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 5
-    page_size_query_param = 'page_size'
-    max_page_size = 100
-
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
-    serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
-    pagination_class = StandardResultsSetPagination
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'content']
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-created_at')
-    serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
-    pagination_class = StandardResultsSetPagination
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-class FeedView(APIView):
-    permission_classes = [IsAuthenticated]
+class FeedView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
-        followed_users = request.user.following.all()
+        following_users = request.user.following.all()
 
-        posts = Post.objects.filter(
-            author__in=followed_users
-        ).order_by('-created_at')
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
 
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
