b96c6cd96af48139666c970a41ffc3fd29edd618
Eric Danquah <danquah@aircellsolutions.com>
2026-02-23 07:40:07 +0000

    Deploying the Django REST API to Production

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 import social_media_api
 from decouple import config
 import dj_database_url
+import os
+
+PORT = os.environ.get("PORT", 8000)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
