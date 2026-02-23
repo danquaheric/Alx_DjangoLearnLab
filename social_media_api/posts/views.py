59a7ef0cac1784b187fb4625b65d93dffe4e4dc0
Eric Danquah <danquah@aircellsolutions.com>
2026-02-23 06:56:32 +0000

    Implementing Notifications and Likes Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,21 +1,17 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from .models import Post
-from .serializers import PostSerializer
 from django.contrib.contenttypes.models import ContentType
 from .models import Post, Like
 from notifications.models import Notification
+
 
 class LikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        post = Post.objects.get(pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
 
-        like, created = Like.objects.get_or_create(
-            user=request.user,
-            post=post
-        )
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
 
         if created:
             Notification.objects.create(
@@ -27,28 +23,3 @@ class LikePostView(generics.GenericAPIView):
             )
 
         return Response({"message": "Post liked"}, status=status.HTTP_200_OK)
-
-
-class UnlikePostView(generics.GenericAPIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def post(self, request, pk):
-        post = Post.objects.get(pk=pk)
-
-        Like.objects.filter(
-            user=request.user,
-            post=post
-        ).delete()
-
-        return Response({"message": "Post unliked"}, status=status.HTTP_200_OK)
-
-class FeedView(generics.GenericAPIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get(self, request):
-        following_users = request.user.following.all()
-
-        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
-
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
